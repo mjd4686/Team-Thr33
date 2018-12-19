e9171a276c1c0c473cf51276fe36916e037fc54c
jsundahl <jsundahl@onshape.com>
2018-12-19 17:19:42 -0500

    add superuser creation to start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 python3 manage.py migrate
+python3 manage.py shell -c 'from django.contrib.auth.models import User
+user=User.objects.create_user("bridges", password="superbridges")
+user.email="email@email.com"
+user.is_superuser=True
+user.is_staff=True
+user.save()'
 python3 manage.py runserver 0.0.0.0:8000
+
